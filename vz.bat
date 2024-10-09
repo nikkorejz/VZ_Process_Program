5f4ec606305239764795790a53c9dcafb79c83b6
Nikita Gromik <nikita.1911@yandex.ru>
2024-10-09 14:24:11 +0800

    Updated modules to version 2024-09-25

--- vz.bat
+++ vz.bat
@@ -1,15 +1,11 @@
-@Echo off
-rem cell_vz1	
-rem source_cell_vz
+rem @Echo off
+cell_vz
 ovpem_vnz	
-rem vzi_val1
-rem vzi_val113_es
-vzi_val3105_es
-svoprof
-vcxpr1<vcxpr1.inp
-forsr1
-korsreda
-vzi_par3_kon
+vzi_val2509
+ svoprof
+ vcxpr1<vcxpr1.inp
+ forsr1
+ korsreda
+ vzi_par3_kon
 rem vzi_par5
-python3 postprocess.py
-@cls
+rem @cls
---
+++ vz_param.bat
@@ -0,0 +1,6 @@
+rem @Echo off
+cell_vz	
+ ovpem_vnz	
+ vzi_val2509
+vzi_par4
+rem @cls
